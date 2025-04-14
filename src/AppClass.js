74b598c3c1d2c8d30d50c265330f0a22adc933c7
AhnYeonSeob94 <ahnpm94@naver.com>
2025-04-14 16:00:54 +0900

    classcomponent add

--- src/AppClass.js
+++ src/AppClass.js
@@ -11,18 +11,30 @@ export default class AppClass extends Component {
             num:1,
             value:0
         };
+        console.log("constructor");
     }
 
     increase = () =>{
         this.setState({counter2: this.state.counter2 + 1, value: this.state.value+1});
+        console.log("increase function", this.state);
+        //counter2 값이 0으로 나옴 비동기로 처리되기때문에 즉각 반영되지않음
     };
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(){
+    console.log("componentDidUpdate", this.state);
+    //counter2 값이 1로 나옴 최신 업데이트 된 값을 받아볼 수 있음.
+  }
 
   render() {
+    console.log("render");
     return (
       <div>
         <div>state:{this.state.counter2}</div>
         <button onClick={this.increase}>클릭!</button>
-        <BoxClass num={this.state.value}/>
+        {this.state.counter2<3 && <BoxClass num={this.state.value}/>}
       </div>
     )
   }
